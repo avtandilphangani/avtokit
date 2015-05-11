41bf596f352b93ce2d82f91674bc3b381702cc86
Avtandil Pangani <avto@telaveli.ge>
2015-05-12 00:46:26 +0400

    Update sample_data.sql

--- extensions/installer/sample_data.sql
+++ extensions/installer/sample_data.sql
@@ -7,19 +7,19 @@ INSERT INTO `@system_menu_item` (`id`, `menu_id`, `parent_id`, `roles`, `name`, 
 INSERT INTO `@page_page` (`id`, `roles`, `url`, `title`, `status`, `content`, `data`) VALUES
 (1, NULL, 'home', 'Home', 1, 'თქვენს ხელთაა PageKit-ის ყველა ინსტრუმენტი და ტექნოლოგიური სიახლე, იმისათვის რომ თამამად შეძლოთ ვებ-საიტის აშენება. ეს გახლავთ ალფა ვერსია, მაგრამ უკვე ძალიან მოხერხებული პლატფორმა პროგრამირებისათვის.
 
-სრული ინფორმაციის მოძიება შეგიძლიათ  [Pagekit-ი  GitHub-ზე](https://github.com/pagekit/pagekit), რომელიც ღააა ყველასათვის. შენიშვნებზე [შესაძლოა მიწერა](https://github.com/pagekit/pagekit/issues?state=open). ჩვენ ყურადღებით განვიხილავთ ყველა თქვენს გამოხმაურებას და იდეას!
+სრული ინფორმაციის მოძიება შეგიძლიათ  [GitHub-ზე](https://github.com/avtandilphangani/avtokit), რომელიც ღააა ყველასათვის. შენიშვნებზე [შესაძლოა მიწერა](https://github.com/avtandilphangani/avtokit/issues?state=open). ჩვენ ყურადღებით განვიხილავთ ყველა თქვენს გამოხმაურებას და იდეას!
 
 ასევე შექმნილია პირველი  [დოკუმენტაცია GitHub-ზე](https://github.com/pagekit/docs) ბევრი საინტერესო და დეტალური ინფორმაციით. გთხოვთ მოგვწეროთ, თუ რამე არ გამოდის ან იპოვეთ რაიმე შეცდომა.
 
-თუ რამე არ მუშაობს, არ მოგერიდოთ შეკითხვა  [ჩვენს ჩეთში](https://www.hipchat.com/giPcIKmrx). ჩვენ რეალურად განვიხილავთ თქვენს აზრს. ჩვენ ვართ ონლაინში ყოველ დღეWe 8:00-დან 18:00-მდე ევროპის დროით.
+თუ რამე არ მუშაობს, არ მოგერიდოთ შეკითხვის დასმა [ჩეთში](https://gitter.im/avtandilphangani/avtokit). ჩვენ რეალურად განვიხილავთ თქვენს აზრს. ჩვენ ვართ ონლაინში ყოველ დღე.
 
 ## დამატებითი რესურსები
 
 - [Pagekit on Twitter](https://twitter.com/pagekit) - მიიღეთ უახლესი ინფორმაცია
-- [Razr on GitHub](https://github.com/pagekit/razr) - Pagekit''s შაბლონების ძრავი
-- [UIkit Website](http://www.getuikit.com) - Pagekit''s front-end framework
+- [Razr on GitHub](https://github.com/pagekit/razr) - Pagekit's შაბლონების ძრავი
+- [UIkit Website](http://www.getuikit.com) - Pagekit's front-end framework
 
-ისიამოვნე პროგრამირებით  Pagekit-ზე!', '{"markdown":"1"}');
+ისიამოვნე პროგრამირებით  Pagekit-ზე! [![Gitter](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/avtandilphangani/avtokit?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge)', '{"markdown":"1"}');
 
 INSERT INTO `@system_widget` (`id`, `roles`, `type`, `title`, `position`, `priority`, `status`, `pages`, `menu_items`, `data`) VALUES
 (1, NULL, 'widget.menu', 'Main Menu', 'navbar', 0, 1, '', NULL, '{"menu":"1","style":"list"}');
