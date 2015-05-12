0b07c533f1fbbc0e75e9bbd7f095488195f0f2fa
avtandilphangani <avto@telaveli.ge>
2015-05-13 03:53:51 +0400

    13 მაისი 	npm-debug.log

--- extensions/installer/sample_data.sql
+++ extensions/installer/sample_data.sql
@@ -4,23 +4,9 @@ INSERT INTO `@system_menu` (`id`, `name`) VALUES
 INSERT INTO `@system_menu_item` (`id`, `menu_id`, `parent_id`, `roles`, `name`, `url`, `priority`, `status`, `depth`, `pages`, `data`) VALUES
 (1, 1, 0, NULL, 'Home', '/', 0, 1, 0, '', '[]');
 
-INSERT INTO `@page_page` (`id`, `roles`, `url`, `title`, `status`, `content`, `data`) VALUES
-(1, NULL, 'home', 'Home', 1, 'თქვენს ხელთაა PageKit-ის ყველა ინსტრუმენტი და ტექნოლოგიური სიახლე, იმისათვის რომ თამამად შეძლოთ ვებ-საიტის აშენება. ეს გახლავთ ალფა ვერსია, მაგრამ უკვე ძალიან მოხერხებული პლატფორმა პროგრამირებისათვის.
 
-სრული ინფორმაციის მოძიება შეგიძლიათ  [GitHub-ზე](https://github.com/avtandilphangani/avtokit), რომელიც ღააა ყველასათვის. შენიშვნებზე [შესაძლოა მიწერა](https://github.com/avtandilphangani/avtokit/issues?state=open). ჩვენ ყურადღებით განვიხილავთ ყველა თქვენს გამოხმაურებას და იდეას!
-
-ასევე შექმნილია პირველი  [დოკუმენტაცია GitHub-ზე](https://github.com/pagekit/docs) ბევრი საინტერესო და დეტალური ინფორმაციით. გთხოვთ მოგვწეროთ, თუ რამე არ გამოდის ან იპოვეთ რაიმე შეცდომა.
-
-თუ რამე არ მუშაობს, არ მოგერიდოთ შეკითხვის დასმა [ჩეთში](https://gitter.im/avtandilphangani/avtokit). ჩვენ რეალურად განვიხილავთ თქვენს აზრს. ჩვენ ვართ ონლაინში ყოველ დღე.
-
-## დამატებითი რესურსები
-
-- [Pagekit on Twitter](https://twitter.com/pagekit) - მიიღეთ უახლესი ინფორმაცია
-- [Razr on GitHub](https://github.com/pagekit/razr) - Pagekit's შაბლონების ძრავი
-- [UIkit Website](http://www.getuikit.com) - Pagekit's front-end framework
-
-ისიამოვნე პროგრამირებით  Pagekit-ზე! [![Gitter](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/avtandilphangani/avtokit?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge)', '{"markdown":"1"}');
-
+INSERT INTO `@page_page` (`id`, `title`, `content`, `url`, `status`, `data`, `roles`) VALUES
+(1, 'Home', 'თქვენს ხელთაა PageKit-ის ყველა ინსტრუმენტი და ტექნოლოგიური სიახლე, იმისათვის რომ თამამად შეძლოთ ვებ-საიტის აშენება. ეს გახლავთ ალფა ვერსია, მაგრამ უკვე ძალიან მოხერხებული პლატფორმა პროგრამირებისათვის.  \r\n\r\nსრული ინფორმაციის მოძიება შეგიძლიათ  [GitHub-ზე](https://github.com/avtandilphangani/avtokit), რომელიც ღააა ყველასათვის.   \r\n\r\nშენიშვნებზე [შესაძლოა მიწერა](https://github.com/avtandilphangani/avtokit/issues?state=open). ჩვენ ყურადღებით განვიხილავთ ყველა თქვენს გამოხმაურებას და იდეას!  \r\n\r\nასევე შექმნილია პირველი  [დოკუმენტაცია GitHub-ზე](https://github.com/pagekit/docs) ბევრი საინტერესო და დეტალური ინფორმაციით. გთხოვთ მოგვწეროთ, თუ რამე არ გამოდის ან იპოვეთ რაიმე შეცდომა.\r\n\r\nთუ რამე არ მუშაობს, არ მოგერიდოთ შეკითხვის დასმა [ჩეთში](https://gitter.im/avtandilphangani/avtokit). ჩვენ რეალურად განვიხილავთ თქვენს აზრს. ჩვენ ვართ ონლაინში ყოველ დღე.\r\n\r\n### დამატებითი რესურსები\r\n\r\n- [Pagekit on Twitter](https://twitter.com/pagekit) - მიიღეთ უახლესი ინფორმაცია\r\n- [Razr on GitHub](https://github.com/pagekit/razr) - Pagekit''s შაბლონების ძრავი\r\n- [UIkit Website](http://www.getuikit.com) - Pagekit''s front-end framework\r\n\r\nისიამოვნე პროგრამირებით  Pagekit-ზე! [![Gitter](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/avtandilphangani/avtokit?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge)', 'home', 1, '{"title":"1","markdown":"1"}', NULL);
 INSERT INTO `@system_widget` (`id`, `roles`, `type`, `title`, `position`, `priority`, `status`, `pages`, `menu_items`, `data`) VALUES
 (1, NULL, 'widget.menu', 'Main Menu', 'navbar', 0, 1, '', NULL, '{"menu":"1","style":"list"}');
 
