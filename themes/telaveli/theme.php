76bfeceed71940f21fafdd5d13cb7cd6d107f5e8
avtandilphangani <avto@telaveli.ge>
2015-05-13 22:55:44 +0400

    13.05 3

--- themes/telaveli/theme.php
+++ themes/telaveli/theme.php
@@ -12,13 +12,22 @@ return [
 
     // positions are sections in your theme where widgets can be published
     'positions' => [
-        'logo'       => 'Logo',
-        // ...
+                'logo-small' => 'Logo Small',
+                'navbar'     => 'Navbar',
+                'top'        => 'Top',
+                'sidebar-a'  => 'Sidebar A',
+                'sidebar-b'  => 'Sidebar B',
+                'footer'     => 'Footer',
+                'offcanvas'  => 'Offcanvas'
     ],
 
     // renderers define the markup that is rendered in widget positions
     'renderer' => [
         'blank'     => 'theme://telaveli/views/renderer/position.blank.razr',
+     //   'grid'      => 'theme://telaveli/views/renderer/position.grid.php',
+        'navbar'    => 'theme://telaveli/views/renderer/position.navbar.razr',
+        'offcanvas' => 'theme://telaveli/views/renderer/position.offcanvas.razr',
+        'panel'     => 'theme://telaveli/views/renderer/position.panel.razr'
         // ...
     ],
 
